9a3b10e60f9bb3f26d9572a52a8b188f1531beac
davidtangness <erictangness@yahoo.ca>
2014-12-12 20:09:21 -0500

    Updated "Moving the player" finished tutorial

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,14 +4,41 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public UnityEngine.UI.Text CounterText;
+	public UnityEngine.UI.Text WinText;
+	private int count;
+
+	void start()
+	{
+		count = 0;
+	}
+
+	void Update()
+	{
+		CounterText.text = "Score: " + count.ToString ();
+		if (count >= 7) 
+		{
+			WinText.text = "YOU WIN!";
+		}
+	}
 
 	void FixedUpdate(){
 		float movementHorizontal = Input.GetAxis("Horizontal");
-		float movementvertical = Input.GetAxis("Vertical");
+		float movementVertical = Input.GetAxis("Vertical");
 
 
-		Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementvertical);
+		Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
 
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+
+		}
 	}
 }
